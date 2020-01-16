6521086510557d1f9e6d6c6f937ccb2ee8bb13a5
Yasmin <Yasmin_bwzr@apps.ipb.ac.id>
2020-01-16 18:37:49 +0700

    page bout

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.template')
+
+@section('title', 'About Us')
+
+@section('breadcrumb')
+    <!-- breadcrumb start-->
+    <!-- MASI HARDCODE :( -->
+    <section class="breadcrumb breadcrumb_bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner">
+                        <div class="breadcrumb_iner_item">
+                            <p>Home / About Us</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb end-->
+@endsection
+
+@section('content')
+    <div class="blog-section">
+        <div class="container">
+            <h1 class="text-center">Ringkasan</h1>
+            <p class="section-description text-center">Situs web ini dioperasikan oleh Zeen By Zi. Di seluruh situs, istilah "kami", 
+                "kami" dan "kami" merujuk ke Zeen By Zi . Zeen menawarkan situs web ini, termasuk semua informasi, alat, dan layanan 
+                yang tersedia dari situs ini kepada Anda, pengguna, tergantung pada penerimaan Anda atas semua persyaratan, ketentuan, 
+                kebijakan, dan pemberitahuan yang disebutkan di sini.</p>
+            <p class="section-description text-center">Kami tidak bertanggung jawab jika informasi yang tersedia di situs ini tidak akurat,
+                 lengkap, atau terkini. Materi di situs ini disediakan untuk informasi umum saja dan tidak boleh diandalkan atau digunakan 
+                 sebagai satu-satunya dasar untuk membuat keputusan tanpa konsultasi sumber informasi utama, lebih akurat, lebih lengkap atau 
+                 lebih tepat waktu. Ketergantungan apa pun pada materi di situs ini adalah risiko Anda sendiri.</p>
+            <p class="section-description text-center">Situs ini mungkin berisi informasi historis tertentu. Informasi historis, tentu saja, 
+                tidak terkini dan hanya disediakan untuk referensi Anda. Kami berhak mengubah isi situs ini kapan saja, tetapi kami tidak 
+                berkewajiban memperbarui informasi apa pun di situs kami. Anda setuju bahwa Anda bertanggung jawab untuk memantau perubahan pada situs kami.</p>
+            
+            <h1 class="text-center">Perubahan Layanan dan Harga</h1>
+            <ul class="section-description">
+                <li>Harga untuk setiap produk  dapat berubah tanpa pemberitahuan.</li>
+                <li>Kami berhak setiap saat untuk memodifikasi atau menghentikan Layanan (atau bagian atau konten apa pun) tanpa pemberitahuan kapan saja.</li>
+                <li>Kami tidak akan bertanggung jawab kepada Anda atau kepada pihak ketiga untuk modifikasi apa pun, perubahan harga, penangguhan, atau penghentian Layanan.</li>
+            </ul>
+
+            <h1 class="text-center">Layanan dan Harga</h1>
+            <ul class="section-description">
+                <li>Produk atau layanan tertentu mungkin tersedia secara eksklusif online melalui situs web. Produk atau layanan ini mungkin memiliki jumlah terbatas dan dapat 
+                    dikembalikan atau ditukarkan hanya berdasarkan Kebijakan Pengembalian kami.</li>
+                <li>Kami telah berusaha semaksimal mungkin untuk menampilkan warna dan gambar produk kami yang muncul di toko. Kami tidak dapat menjamin bahwa tampilan
+                     monitor komputer Anda dengan warna apa pun akan akurat.</li>
+                <li>Kami berhak, tetapi tidak berkewajiban, untuk membatasi penjualan produk atau Layanan kami kepada siapa pun, wilayah geografis atau yurisdiksi. 
+                    Kami dapat menggunakan hak ini berdasarkan kasus per kasus. Kami berhak untuk membatasi jumlah produk atau layanan yang kami tawarkan. Semua 
+                    deskripsi produk atau harga produk dapat berubah kapan saja tanpa pemberitahuan, atas kebijakan kami sendiri. Kami berhak untuk menghentikan 
+                    produk apa pun setiap saat. Setiap tawaran untuk produk atau layanan apa pun yang dibuat di situs ini tidak berlaku jika dilarang.</li>
+                <li>Kami tidak menjamin bahwa kualitas produk, layanan, informasi, atau materi lainnya yang dibeli atau diperoleh oleh Anda akan memenuhi harapan Anda, 
+                    atau bahwa kesalahan apa pun dalam Layanan akan diperbaiki.</li>
+            </ul>
+
+            <h1 class="text-center">Akurasi Billing dan Informasi Akun</h1>
+            <ul class="section-description">
+                <li>Kami berhak menolak pesanan apa pun yang Anda lakukan kepada kami. Kami dapat, dengan kebijakan kami sendiri, membatasi atau membatalkan 
+                    jumlah yang dibeli per orang, per rumah tangga atau per pesanan. Pembatasan ini dapat mencakup pesanan yang ditempatkan oleh atau di 
+                    bawah akun pelanggan yang sama, kartu kredit yang sama, dan / atau pesanan yang menggunakan alamat penagihan dan / atau pengiriman yang sama. 
+                    Jika kami melakukan perubahan atau membatalkan pesanan, kami dapat mencoba memberi tahu Anda dengan menghubungi e-mail dan / atau alamat penagihan 
+                    / nomor telepon yang diberikan pada saat pesanan dibuat. Kami berhak untuk membatasi atau melarang pesanan yang, menurut penilaian kami, tampaknya 
+                    ditempatkan oleh dealer, reseller atau distributor.</li>
+                <li>Anda setuju untuk memberikan informasi pembelian dan akun terkini, lengkap dan akurat untuk semua pembelian yang dilakukan di toko kami. Anda 
+                    setuju untuk segera memperbarui akun Anda dan informasi lainnya, termasuk alamat email Anda dan nomor kartu kredit dan tanggal kedaluwarsa, 
+                    sehingga kami dapat menyelesaikan transaksi Anda dan menghubungi Anda jika diperlukan.</li>
+                <li>Untuk detail lebih lanjut, silakan tinjau Kebijakan Pengembalian kami.</li>
+            </ul>
+        </div>
+    </div>
+@endsection
